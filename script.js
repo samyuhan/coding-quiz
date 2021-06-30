aeb86cbcd7a5bb35160c9362161a6e28ee18fb81
Samantha Yuhan <sysweettooth1@gmail.com>
2021-06-29 22:29:37 -0700

    duplicate question 1 for question 3

--- script.js
+++ script.js
@@ -114,14 +114,14 @@ var askQ2 = function() {
         answerResponse.textContent = "Wrong!";
         mainEl.appendChild(answerResponse);
         question.remove();
-        //askQ3();
+        askQ3();
     }
 
     // Add a section for the second question
     question.className = "question";
     mainEl.appendChild(question);
 
-    // Add a h1 element that shows the first question
+    // Add a h1 element that shows the second question
     q1.textContent = "The condition in an if / else statement is enclosed within ____.";
     question.appendChild(q1);
 
@@ -146,13 +146,69 @@ var askQ2 = function() {
         answerResponse.textContent = "Correct!";
         mainEl.appendChild(answerResponse);
         question.remove();
-        //askQ3();
+        askQ3();
     });
 
     // Add a button for fourth answer choice
     a4.textContent = "4. square brackets";
     answers.appendChild(a4);
     a4.addEventListener("click", wrongAnswer);
+}
+
+var askQ3 = function() {
+    // Create local variables
+    var question = document.createElement("section");
+    var q1 = document.createElement("h1");
+    var answers = document.createElement("section");
+    var a1 = document.createElement("button");
+    var a2 = document.createElement("button");
+    var a3 = document.createElement("button");
+    var a4 = document.createElement("button");
+
+    // Function for when user clicks wrong answer choice
+    var wrongAnswer = function() {
+        answerResponse.textContent = "Wrong!";
+        mainEl.appendChild(answerResponse);
+        question.remove();
+        //askQ4();
+    }
+
+    // Add a section for the third question
+    question.className = "question";
+    mainEl.appendChild(question);
+
+    // Add a h1 element that shows the third question
+    q1.textContent = "Arrays in JavaScript can be used to store ____.";
+    question.appendChild(q1);
+
+    // Add a section for the answers
+    answers.className = "answers";
+    question.appendChild(answers);
+
+    // Add a button for first answer choice
+    a1.textContent = "1. numbers and strings";
+    answers.appendChild(a1);
+    a1.addEventListener("click", wrongAnswer);
+
+    // Add a button for second answer choice
+    a2.textContent = "2. other arrays";
+    answers.appendChild(a2);
+    a2.addEventListener("click", wrongAnswer);
+
+    // Add a button for third answer choice
+    a3.textContent = "3. booleans";
+    answers.appendChild(a3);
+    a3.addEventListener("click", wrongAnswer);
+
+    // Add a button for fourth answer choice
+    a4.textContent = "4. all of the above";
+    answers.appendChild(a4);
+    a4.addEventListener("click", function() {
+        answerResponse.textContent = "Correct!";
+        mainEl.appendChild(answerResponse);
+        question.remove();
+        //askQ4();
+    });
 }
 
 startQuiz();
