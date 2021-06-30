8d4fc7340fdb1b488499989817cb18dcc0a53f2c
Samantha Yuhan <sysweettooth1@gmail.com>
2021-06-29 20:13:22 -0700

    create start quiz button

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ var secondsLeft = 75;
 
 // Function for start page
 var startQuiz = function() {
-    // Create a div element for the start page
+    // Create local variables to create the elements for start page
     var startpage = document.createElement("div");
+    var title = document.createElement("h1");
+    var description = document.createElement("p");
+    var startBtn = document.createElement("button");
+    
+    // Add a div element for the start page
     startpage.className = "startpage"
     mainEl.appendChild(startpage);
 
-    // Create a h1 element for the quiz title
-    var title = document.createElement("h1");
+    // Add a h1 element for the quiz title
     title.className = "quiz-title";
     title.textContent = "Coding Quiz Challenge";
     startpage.appendChild(title);
 
-    // Create a p element for the quiz description
-    var description = document.createElement("p");
+    // Add a p element for the quiz description
     description.className = "quiz-description";
     description.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answer will penalize your score/time by ten seconds!"
     startpage.appendChild(description);
+
+    // Add button to start quiz
+    startBtn.className = "start-btn";
+    startBtn.textContent = "Start Quiz";
+    container.appendChild(startBtn);
 }
