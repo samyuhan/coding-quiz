41d7505d45248ecf00b1c0a0ab2c0cf08e012244
Samantha Yuhan <sysweettooth1@gmail.com>
2021-06-29 20:51:57 -0700

    remove start page when user clicks on start button

--- script.js
+++ script.js
@@ -13,21 +13,29 @@ var startQuiz = function() {
     var startBtn = document.createElement("button");
     
     // Add a div element for the start page
-    startpage.className = "startpage"
+    startpage.id = "startpage"
     mainEl.appendChild(startpage);
 
     // Add a h1 element for the quiz title
-    title.className = "quiz-title";
+    title.id = "quiz-title";
     title.textContent = "Coding Quiz Challenge";
     startpage.appendChild(title);
 
     // Add a p element for the quiz description
-    description.className = "quiz-description";
+    description.id = "quiz-description";
     description.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answer will penalize your score/time by ten seconds!"
     startpage.appendChild(description);
 
     // Add button to start quiz
-    startBtn.className = "start-btn";
+    startBtn.id = "start-btn";
     startBtn.textContent = "Start Quiz";
-    container.appendChild(startBtn);
+    startpage.appendChild(startBtn);
+
+    // Add an event listener to remove the content in startpage when the button is clicked
+    startBtn.addEventListener("click", function() {
+        title.remove();
+        description.remove();
+        startBtn.remove();
+    });
 }
+startQuiz();
